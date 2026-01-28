affb6b62338985924f790c12a50cf0641e6b814b
Taiki Kobayashi <taikik@umich.edu>
2026-01-28 00:31:33 -0500

    attempting to solve video size too big error.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,8 +22,9 @@ const PORT = 3000;
 const PgSession = connectPgSimple(session);
 
 //Parse body before routes
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Increase limit to 50mb (or whatever size you need)
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // // Middleware
 // app.use(cors({
